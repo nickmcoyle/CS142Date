1958c97337318c701282fc076fefac789ff886d3
nickmcoyle <nickmcoyle@gmail.com>
2018-10-17 13:23:08 -0700

    first commit

---
+++ MyDate.java
@@ -0,0 +1,35 @@
+
+/**
+ * Write a description of class MyDate here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class MyDate
+{
+    private int day;
+    private int month;
+    private int year;
+    
+    public MyDate() {
+        this(1,1,2000);   
+    }
+    
+    public MyDate(int month, int day, int year) {
+        setMonth(month);
+        setDay(day);
+        setYear(year);
+    }
+    
+    public void setMonth(int month) {
+        
+    }
+    
+    public void setDay(int day) {
+        
+    }
+    
+    public void setYear(int year) {
+        
+    }    
+}
